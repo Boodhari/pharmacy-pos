40bed1def6cac4819624177fe71e413a97d10e19
Abadir Hassan <34312697+Boodhari@users.noreply.github.com>
2025-07-10 22:50:39 +0300

    Update db.php

--- pharmacy/config/db.php
+++ pharmacy/config/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root ", "", "smart_pharmacy");
+$conn = new mysqli("sql8.freesqldatabase.com", "sql8789367 ", "FAVAsfUJqS", "sql8789367");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
